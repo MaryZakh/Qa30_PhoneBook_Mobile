aaacc41aa190ab3bc449f61d76f4d33eb3c1f7dd
Maria Z <z0559882272@gmail.com>
2025-09-14 19:04:03 +0300

    added login negative

--- src/main/java/screens/AuthenticationScreen.java
+++ src/main/java/screens/AuthenticationScreen.java
@@ -3,7 +3,11 @@ package screens;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import models.Auth;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class AuthenticationScreen extends BaseScreen{
     public AuthenticationScreen(AppiumDriver<AndroidElement> driver) {
@@ -38,10 +42,24 @@ public class AuthenticationScreen extends BaseScreen{
         return new ContactListScreen(driver);
     }
 
+    public AuthenticationScreen submitLoginNegative(){
+        loginBtn.click();
+        return this;
+    }
+
     public AuthenticationScreen fillLoginRegistrationForm(Auth auth) {
         should(emailEditText,10);
         type(emailEditText, auth.getEmail());
         type(passwordEditText, auth.getPassword());
+        return this;
+    }
+
+    public AuthenticationScreen isErrorMessageHasText(String text) {
+        Alert alert = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert();
+        Assert.assertTrue(alert.getText().contains(text));
+        alert.accept();
         return this;
     }
 }
