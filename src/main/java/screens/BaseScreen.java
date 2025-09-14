34b32db80cefc4b136ed803b0c706ee874955661
Maria Z <z0559882272@gmail.com>
2025-09-14 20:59:18 +0300

    added Add New Contact tests (successAll && empty name)

--- src/main/java/screens/BaseScreen.java
+++ src/main/java/screens/BaseScreen.java
@@ -3,9 +3,11 @@ package screens;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class BaseScreen {
     AppiumDriver<AndroidElement> driver;
@@ -21,6 +23,7 @@ public class BaseScreen {
         if(text!=null){
             element.sendKeys(text);
         }
+        driver.hideKeyboard();
     }
 
     public boolean isShouldHave(AndroidElement element,String text, int time){
@@ -39,4 +42,13 @@ public class BaseScreen {
     public void should(AndroidElement element,int time){
         new WebDriverWait(driver,time).until(ExpectedConditions.visibilityOf(element));
     }
+
+    public void checkAlertText(String text){
+        Alert alert = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert();
+        Assert.assertTrue(alert.getText().contains(text));
+        alert.accept();
+    }
+
 }
--- src/main/java/screens/ContactListScreen.java
+++ src/main/java/screens/ContactListScreen.java
@@ -2,8 +2,11 @@ package screens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
+
+import java.util.List;
 
 public class ContactListScreen extends BaseScreen {
     public ContactListScreen(AppiumDriver<AndroidElement> driver) {
@@ -12,10 +15,18 @@ public class ContactListScreen extends BaseScreen {
 
     @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/action_bar']/android.widget.TextView")
     AndroidElement activityTextView;
+
     @FindBy(xpath = "//*[@content-desc='More options']")
     AndroidElement menuOptions;
+
     @FindBy(xpath = "//*[@text='Logout']")
     AndroidElement logoutButton;
+
+    @FindBy(xpath = "//*[@content-desc = 'add']")
+    AndroidElement plusBtn;
+
+    @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/rowName']")
+    List<AndroidElement> contactNameList;
 
 
     public boolean isActivityTitleDisplayed(String text) {
@@ -31,9 +42,31 @@ public class ContactListScreen extends BaseScreen {
         return new AuthenticationScreen(driver);
     }
 
-    public ContactListScreen isAccountOpened(){
+    public ContactListScreen isAccountOpened() {
         Assert.assertTrue(isActivityTitleDisplayed("Contact list"));
         return this;
     }
 
+    public AddNewContactScreen openContactForm() {
+        if(activityTextView.getText().equals("Contact list")) {
+            plusBtn.click();
+        }
+        return new AddNewContactScreen(driver);
+    }
+
+    public ContactListScreen isContactAddedByName(String name, String lastName) {
+        //List<AndroidElement> list = driver.findElements(By.xpath(""));
+        isShouldHave(activityTextView,"Contact list",5);
+        System.out.println("size of list" + contactNameList.size());
+        boolean isPresent = false;
+
+        for (AndroidElement el:contactNameList){
+            if(el.getText().equals(name + " "+lastName)){
+                isPresent=true;
+                break;
+            }
+        }
+        Assert.assertTrue(isPresent);
+        return this;
+    }
 }
