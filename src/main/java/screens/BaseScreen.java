04f9942f6b1b1ab5e19e4e9451b888a8adcff46e
Maria Z <z0559882272@gmail.com>
2025-09-17 19:09:15 +0300

    added delete first contact

--- src/main/java/screens/BaseScreen.java
+++ src/main/java/screens/BaseScreen.java
@@ -9,29 +9,31 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.util.List;
+
 public class BaseScreen {
     AppiumDriver<AndroidElement> driver;
 
     public BaseScreen(AppiumDriver<AndroidElement> driver) {
         this.driver = driver;
-        PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
-    public void type(AndroidElement element,String text){
+    public void type(AndroidElement element, String text) {
         element.click();
         element.clear();
-        if(text!=null){
+        if (text != null) {
             element.sendKeys(text);
         }
         driver.hideKeyboard();
     }
 
-    public boolean isShouldHave(AndroidElement element,String text, int time){
-        return new WebDriverWait(driver,time)
-                .until(ExpectedConditions.textToBePresentInElement(element,text));
+    public boolean isShouldHave(AndroidElement element, String text, int time) {
+        return new WebDriverWait(driver, time)
+                .until(ExpectedConditions.textToBePresentInElement(element, text));
     }
 
-    public void pause(int time){
+    public void pause(int time) {
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
@@ -39,16 +41,30 @@ public class BaseScreen {
         }
     }
 
-    public void should(AndroidElement element,int time){
-        new WebDriverWait(driver,time).until(ExpectedConditions.visibilityOf(element));
+    public void should(AndroidElement element, int time) {
+        new WebDriverWait(driver, time).until(ExpectedConditions.visibilityOf(element));
     }
 
-    public void checkAlertText(String text){
-        Alert alert = new WebDriverWait(driver,10)
+    public void checkAlertText(String text) {
+        Alert alert = new WebDriverWait(driver, 10)
                 .until(ExpectedConditions.alertIsPresent());
         driver.switchTo().alert();
         Assert.assertTrue(alert.getText().contains(text));
         alert.accept();
     }
+
+    public boolean isElementDisplayed(AndroidElement element) {
+        try {
+            should(element, 5);
+            return element.isDisplayed();
+        } catch (IllegalAccessError e) {
+            return false;
+        }
+    }
+
+    public boolean isElementPresentInList(List<AndroidElement>list){
+        return list.size()>0;
+    }
+
 
 }
--- src/main/java/screens/ContactListScreen.java
+++ src/main/java/screens/ContactListScreen.java
@@ -1,8 +1,11 @@
 package screens;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
@@ -19,6 +22,9 @@ public class ContactListScreen extends BaseScreen {
     @FindBy(xpath = "//*[@content-desc='More options']")
     AndroidElement menuOptions;
 
+    @FindBy(xpath = "//*[@content-desc='More options']")
+    List<AndroidElement> menuOptionsList;
+
     @FindBy(xpath = "//*[@text='Logout']")
     AndroidElement logoutButton;
 
@@ -27,6 +33,15 @@ public class ContactListScreen extends BaseScreen {
 
     @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/rowName']")
     List<AndroidElement> contactNameList;
+
+    @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/rowContainer']")
+    List<AndroidElement> contactList;
+
+    @FindBy(id = "android:id/button1")
+    AndroidElement yesBtn;
+
+    int countBefore;
+    int countAfter;
 
 
     public boolean isActivityTitleDisplayed(String text) {
@@ -42,13 +57,29 @@ public class ContactListScreen extends BaseScreen {
         return new AuthenticationScreen(driver);
     }
 
+    public AuthenticationScreen logout2() {
+        if (isElementDisplayed(menuOptions)) {
+            menuOptions.click();
+            logoutButton.click();
+        }
+        return new AuthenticationScreen(driver);
+    }
+
+    public AuthenticationScreen logout3() {
+        if (isElementPresentInList(menuOptionsList)) {
+            menuOptions.click();
+            logoutButton.click();
+        }
+        return new AuthenticationScreen(driver);
+    }
+
     public ContactListScreen isAccountOpened() {
         Assert.assertTrue(isActivityTitleDisplayed("Contact list"));
         return this;
     }
 
     public AddNewContactScreen openContactForm() {
-        if(activityTextView.getText().equals("Contact list")) {
+        if (activityTextView.getText().equals("Contact list")) {
             plusBtn.click();
         }
         return new AddNewContactScreen(driver);
@@ -56,17 +87,48 @@ public class ContactListScreen extends BaseScreen {
 
     public ContactListScreen isContactAddedByName(String name, String lastName) {
         //List<AndroidElement> list = driver.findElements(By.xpath(""));
-        isShouldHave(activityTextView,"Contact list",5);
+        isShouldHave(activityTextView, "Contact list", 5);
         System.out.println("size of list" + contactNameList.size());
         boolean isPresent = false;
 
-        for (AndroidElement el:contactNameList){
-            if(el.getText().equals(name + " "+lastName)){
-                isPresent=true;
+        for (AndroidElement el : contactNameList) {
+            if (el.getText().equals(name + " " + lastName)) {
+                isPresent = true;
                 break;
             }
         }
         Assert.assertTrue(isPresent);
         return this;
     }
+
+
+    public ContactListScreen deleteFirstContact() {
+        isActivityTitleDisplayed("Contact list");
+        countBefore = contactList.size();
+        System.out.println(countBefore);
+        AndroidElement first = contactList.get(0);
+        Rectangle rectangle = first.getRect();
+        int xFrom = rectangle.getX() + rectangle.getWidth() / 8;
+        //int xTo= rectangle.getX()+(rectangle.getWidth()/8)*7;
+        int xTo = rectangle.getWidth() - xFrom;
+        int y = rectangle.getY() + rectangle.getHeight() / 2;
+
+        TouchAction<?> touchAction = new TouchAction<>(driver);
+        touchAction.longPress(PointOption.point(xFrom, y))
+                .moveTo(PointOption.point(xTo, y))
+                .release().perform();
+        should(yesBtn,8);
+        yesBtn.click();
+        pause(3000);
+        countAfter = contactList.size();
+        System.out.println(countAfter);
+
+        return this;
+    }
+    public ContactListScreen isListSizeLessOnOne(){
+        Assert.assertEquals(countBefore-countAfter,1);
+        return this;
+    }
+
+
 }
